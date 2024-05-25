5e8b39aa926b127742ff337de2203812bd36bc62
YoungJin <101448204+AI-WonYJ@users.noreply.github.com>
2024-05-25 20:03:18 +0900

    Update first.py

--- first.py
+++ first.py
@@ -1,33 +1,31 @@
-def generate_pattern(arr):
-    n = len(arr)
-    
-    setting = []
-    for k in range(0, len(arr)):
-        setting.append(arr[k][1])
+def total(arr):
+    summ = 0
+    for i in range(0, len(arr)):
+        summ += arr[i][0]*arr[i][1]*arr[i][2]
+    return summ
 
+def increment_array(arr):
     while True:
-        
-        sum = 0
-        answer = []
-        for j in range(0, len(arr)):
-            sum += arr[j][0]*(arr[j][1]*arr[j][2])
-        if sum == 600000:
-            answer.append(arr)
-            print(arr)
-        print(arr, sum)
-        
-        i = n - 1
-        while i >= 0 and sum > 600000:
+        i = len(arr) - 1
+        if total(arr) == 700000:
+            print(arr, total(arr))
+        while i >= 0 and total(arr) >= 700000:
             arr[i][1] = 1
             i -= 1
-
         if i < 0:
             break
-        
         arr[i][1] += 1
-    
-    print("\n\nanswer\n\n")
-    print(answer)
 
-# 예제로 배열을 [4, 3, 2, 1]로 설정하여 패턴 생성
-generate_pattern([[1250, 1, 12], [1030, 1, 20], [520, 1, 10], [680, 1, 40], [2150, 1, 10], [1030, 1, 24], [720, 1, 24], [790, 1, 24], [710, 1, 24], [680, 1, 24], [300, 1, 24]])
+increment_array([[1250, 2, 12], 
+[1030, 4, 20],
+[520, 4, 20],
+[680, 4, 30],
+[2150, 1, 24],
+[1030, 1, 16],
+[720, 4, 16],
+[790, 1, 16],
+[710, 1, 24],
+[680, 1, 30],
+[300, 4, 30]])
+
+print("END")
